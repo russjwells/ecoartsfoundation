a0232b84ddfafa1b59b6472abc01a5996310b9d1
Russ Wells <russ@russdigital.com>
2017-06-06 13:51:32 -0700

    spelling

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="Header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Eco Arts Foundation</h2>
+          <h2>EcoArts Foundation</h2>
         </div>
         <p className="App-intro">
           Welcome to the Eco Arts Foundation. More coming soon.
